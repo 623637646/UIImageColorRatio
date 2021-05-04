b5010f3fce3dbb88aaf6eafdc4154da683588b6e
Yanni Wang <yanni.wang@Yannis-MacBook-Pro.local>
2021-05-04 18:12:32 +0800

    Analysis algorithm.

--- ImageAnalyzer/ImageAnalyzer.swift
+++ ImageAnalyzer/ImageAnalyzer.swift
@@ -7,6 +7,32 @@
 
 import UIKit
 
-public func analyze(image: UIImage) {
-    
+public struct Color: Hashable {
+    public let r: UInt8
+    public let g: UInt8
+    public let b: UInt8
+}
+
+extension UIImage {
+    public func analyze() -> [(color: Color, rate: Float)] {
+        
+        // TODO: test the releasing.
+        guard let pixelData = self.cgImage?.dataProvider?.data else {
+            return []
+        }
+        let length: Int = Int(CFDataGetLength(pixelData))
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        var dic = [Color: UInt64]()
+        var totalCount = 0
+        for index in 0 ... (length / 4 - 1) {
+            let r = data[index * 4]
+            let g = data[index * 4 + 1]
+            let b = data[index * 4 + 2]
+            let color = Color.init(r: r, g: g, b: b)
+            dic[color] = (dic[color] ?? 0) + 1
+            totalCount += 1
+        }
+        let result = dic.map { (color: $0, rate: Float($1) / Float(totalCount)) }.sorted { $0.rate > $1.rate }
+        return result
+    }
 }
--- ImageAnalyzerExample/ImageAnalyzerExample/ViewController.swift
+++ ImageAnalyzerExample/ImageAnalyzerExample/ViewController.swift
@@ -8,17 +8,58 @@
 import UIKit
 import Eureka
 import ViewRow
+import ImageAnalyzer
 
 class ViewController: FormViewController {
+    
+    let originalImage = UIImage(named: "bird")!
+    
+    var filteredImage = UIImage(named: "bird")!
+    
+    var analysisResult = [(color: Color, rate: Float)]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         form +++ Section("Original image")
-            <<< getImageRow(image: UIImage(named: "bird")!)
-            +++ Section("Analyzed image")
-            <<< getImageRow(image: UIImage(named: "bird")!)
-            +++ Section("Config")
+            <<< getImageRow(image: originalImage)
+            +++ Section("Filtered image")
+            <<< getImageRow(image: filteredImage)
+            +++ Section("Analysis")
+            <<< LabelRow(){
+                $0.title = "Number of colors"
+                $0.value = String(analysisResult.count)
+                $0.tag = "numberOfColors"
+            }
+            +++ Section("Top 10 colors") {
+                $0.tag = "top10Colors"
+            }
+        
+        analyze()
+    }
+    
+    func analyze() {
+        analysisResult = originalImage.analyze()
+        reload()
+    }
+    
+    func reload() {
+        if let row = self.form.rowBy(tag: "numberOfColors") as? LabelRow {
+            row.value = String(analysisResult.count)
+        }
+        if let top10ColorsSection = self.form.sectionBy(tag: "top10Colors") {
+            top10ColorsSection.removeAll()
+            for index in 0 ... min(10 - 1, analysisResult.count - 1) {
+                let data = analysisResult[index]
+                top10ColorsSection
+                    <<< LabelRow(){
+                        $0.value = String.init(format: "%0.2f%%", data.rate * 100)
+                    }.cellUpdate({ (cell, row) in
+                        cell.backgroundColor = UIColor.init(red: CGFloat(data.color.r) / 255, green: CGFloat(data.color.g) / 255, blue: CGFloat(data.color.b) / 255, alpha: 1)
+                    })
+            }
+        }
+        
     }
     
     func getImageRow(image: UIImage) -> ViewRow<UIImageView> {
