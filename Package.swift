a4979e0ac47beff60480dc7e53811000b73a96ee
Yanni Wang <yanni.wang@yannis-mbp.lan>
2021-06-16 21:13:29 +0800

    [SPM] support swift package manager.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UIImageColorRatio",
+    platforms: [.iOS(.v10)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "UIImageColorRatio",
+            targets: ["UIImageColorRatio"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
+        // Source Code
+        .target(
+            name: "UIImageColorRatio",
+            path: "UIImageColorRatio",
+            exclude: ["UIImageColorRatio.h", "Info.plist"]),
+    ]
+)
